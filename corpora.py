d3c7683257ad96d8b1791a8c17d54ddf06fcdff4
Davidbestion <104589703+Davidbestion@users.noreply.github.com>
2024-10-03 00:04:21 -0400

    chore: Update .gitignore and nltk corpora
    - Added .venv/ and __pycache__/ to .gitignore
    - Updated nltk corpora by downloading additional resources (all, punkt_tab, vader_lexicon, stopwords, wordnet)

--- corpora.py
+++ corpora.py
@@ -1,5 +1,7 @@
 import nltk
 
-# nltk.download('all')
-# nltk.download('punkt_tab')
+nltk.download('all')
+nltk.download('punkt_tab')
 nltk.download('vader_lexicon')
+nltk.download('stopwords')
+nltk.download('wordnet')
